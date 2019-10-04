2d307f3cd80523c78d15e1ed9f274e0920b73021
Ayan Yenbekbay <ayan.yenb@gmail.com>
2019-10-05 00:05:27 +0100

    fix(typescript): disable/loosen up unhelpful rules

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -43,6 +43,7 @@ module.exports = {
       },
     ],
     // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin
+    '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
