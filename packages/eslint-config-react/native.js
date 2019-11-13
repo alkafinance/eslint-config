402341606f9096d86a1e2c8e0de84402d13ebddc
Ayan Yenbekbay <ayan.yenb@gmail.com>
2019-11-13 13:29:41 +0000

    feat(react): add new react-native rules

--- packages/eslint-config-react/native.js
+++ packages/eslint-config-react/native.js
@@ -44,6 +44,7 @@ module.exports = {
     'react-native/no-color-literals': 'off',
     'react-native/no-inline-styles': 'off',
     'react-native/no-raw-text': 'off', // buggy (https://github.com/Intellicode/eslint-plugin-react-native/issues/210)
+    'react-native/no-single-element-style-arrays': 'warn',
     'react-native/no-unused-styles': 'warn',
     'react-native/sort-styles': 'off',
     'react-native/split-platform-components': 'warn',
